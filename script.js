3a4e72f00256e25a2496d84df6254bc95e5ada57
mtsluis <151209805+mtsluis@users.noreply.github.com>
2024-02-26 15:14:35 +0000

    added dog element and some animatios for walk and smell

---
+++ script.js
@@ -0,0 +1,22 @@
+const boardElement = document.getElementById("background")
+
+
+
+
+
+
+
+
+
+
+//Visual methods
+
+
+const dogWalkIntro = () => {
+    const dogElement = document.createElement('div');
+    dogElement.setAttribute("id", "dog");
+
+    boardElement.appendChild(dogElement);
+}
+
+
